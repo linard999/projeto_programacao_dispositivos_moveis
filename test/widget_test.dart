510b6c68e5f5b457d66a8a9a5c09426fa116ca0b
RenatoPorfirio <renatoporfiriox@gmail.com>
2024-03-24 16:58:12 -0300

    pacote adicionado e teste atualizado

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,12 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:projeto_programacao_dispositivos_moveis/main.dart';
+import 'package:prog_disp_moveis_tjrs/prog_disp_moveis_tjrs.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  group('Teste', () {
+    test('Teste de Repository', () async {
+      final repository = ConsultaRepository();
+      final registros = await repository.getRegistros("1", "2022", "3", "2022");
+      expect(registros, isNull);
+    });
   });
 }
